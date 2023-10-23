ab976122d9579ca51b6deef635884efdc3ad427b
oskar åhling <oskar.ahling@edu.edugrade.se>
2023-10-23 15:44:44 +0200

    Add project files.

---
+++ Application/DependancyInjection.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application
+{
+    public static class DependancyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+
+            var assembly = typeof(DependancyInjection).Assembly;
+
+            services.AddMediatR(configuration =>
+                    configuration.RegisterServicesFromAssemblies(
+                        typeof(DependancyInjection).Assembly));
+
+            services.AddValidatorsFromAssembly(assembly);
+
+            return services;
+
+        }
+    }
+}
